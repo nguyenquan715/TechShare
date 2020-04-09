dc2846e2e20ef4f11448b7b17040ad46b12c93f4
Nguyễn Anh Quân <nguyenanhquan715@gmail.com>
2020-04-09 18:33:07 +0700

    Add view

---
+++ TechShare/TechShare/Controllers/ManagerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechShare.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Member()
+        {
+            return View();
+        }
+        public IActionResult Employee()
+        {
+            return View();
+        }
+        public IActionResult Administrator()
+        {
+            return View();
+        }
+    }
+}
